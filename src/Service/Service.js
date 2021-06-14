669fea22e42d2aba9f8c436576de100bcebf49f7
Niklas von Heyden <niklas.heyden@me.com>
2021-06-14 18:09:02 +0200

    hotfix

--- src/Service/Service.js
+++ src/Service/Service.js
@@ -10,7 +10,7 @@ function Service() {
       <div className="service_wrapper">
         <SectionHeadline title='Website Konfigurator //' text='Ihre individuelle Website in 5 Minuten' />
         <div className="steps_wrapper">
-          <Step title='Konfigurator' color='#68E1BC' text='Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam.' />
+          <Step title='Konfigurator' color='#68E1BC' text='Loremi ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam.' />
           <Step title='Design' color='#E94E29' text='Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam.' />
           <Step title='Entwicklung' color='#063DFF' text='Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam.' />
           <Step title='Übergabe' color='#111111' text='Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam.' />
